b6dba5474a87b53f4b1b23841e61d3d3671b02e3
mayuko-okamoto <astrology0928@icloud.com>
2021-01-03 12:48:57 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,            presence: true
+  validates :email,               presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :encrypted_password,  presence: true,
+                                  length: { minimum: 6 },
+                                  format: {
+                                    with: VALID_PASSWORD_REGEX
+                                  }
+  validates :first_name,          presence: true
+  validates :last_name,           presence: true
+  validates :first_name_kana,     presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
+  validates :last_name_kana,      presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
+  validates :birth_date,          presence: true
+
+  has_many :items
+  has_many :purchase_records
+  
+
 end
