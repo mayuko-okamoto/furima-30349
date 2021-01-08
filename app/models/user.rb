d4f2a8dd031400894d8dee132cd37ed381728957
mayuko-okamoto <astrology0928@icloud.com>
2021-01-08 18:50:59 +0900

    バリデーションまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname,            presence: true
-  validates :email,               presence: true, uniqueness: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/
-  validates :password,            presence: true,format: { with: VALID_PASSWORD_REGEX }
-  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角で入力して下さい。' }
-  validates :last_name,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角で入力して下さい。' }
-  validates :first_name_kana,     presence: true, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/, message: 'は全角カタカナで入力して下さい。' }
-  validates :last_name_kana,      presence: true, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/, message: 'は全角カタカナで入力して下さい。' }
-  validates :birth_date,          presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :email,               uniqueness: true
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/
+    validates :password,            format: { with: VALID_PASSWORD_REGEX }
+    validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角で入力して下さい。' }
+    validates :last_name,           format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角で入力して下さい。' }
+    validates :first_name_kana,     format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/, message: 'は全角カタカナで入力して下さい。' }
+    validates :last_name_kana,      format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/, message: 'は全角カタカナで入力して下さい。' }
+    validates :birth_date,          
+  end
 
   has_many :items
   has_many :purchase_records
