5447072fc479d1bd8ef0c335a979e97e152090f2
mayuko-okamoto <astrology0928@icloud.com>
2021-01-09 19:51:30 +0900

    Active Storageの導入再掲

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,11 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
 end
