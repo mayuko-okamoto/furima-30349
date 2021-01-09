a585bb80457322662d0c2b6379f74cd1e2bc41f2
mayuko-okamoto <astrology0928@icloud.com>
2021-01-09 21:34:20 +0900

    itemモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,12 @@
 class Item < ApplicationRecord
 
   with_options presence: true do
+    validates :image
     validates :title
     validates :detail
-    validates :price,         inclusion: { in: 300..9999999 }
+    validates :price,         
+      inclusion: { in: 300..9999999 },
+      format: { with: /^[0-9]+$/, message: 'は半角数字で入力して下さい。' }
     validates :category_id
     validates :condition_id
     validates :shipping_payer_id
@@ -15,12 +18,13 @@ class Item < ApplicationRecord
   has_one :purchase_record
   has_one_attached :image
 
-
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :shipping_payer_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :storage_date_id, numericality: { other_than: 1 } 
+ 
+  with_options numericality: { other_than: 1 }
+    validates :category_id
+    validates :condition_id
+    validates :shipping_payer_id
+    validates :prefecture_id
+    validates :storage_date_id
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
