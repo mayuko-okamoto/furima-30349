f6bf9fd685588ec0397ab92db48a39ae4aa3fa7a
mayuko-okamoto <astrology0928@icloud.com>
2021-01-10 23:14:08 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,20 @@
 FactoryBot.define do
   factory :item do
+
+    title                 { Faker::Lorem.characters(40) }
+    detail                { Faker::Lorem.sentence }
+    price                 { Faker::Number.between(from: 300, to: 9999999) }
+    category_id           { 2 }
+    condition_id          { 2 }
+    shipping_payer_id     { 2 }
+    prefecture_id         { 2 }
+    storage_date_id       { 2 }
+    
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
+
