4a12eb3bd8c68ca18ece369b2d04a959be0c57a7
mayuko-okamoto <astrology0928@icloud.com>
2021-01-24 17:04:19 +0900

    バリデーション修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,4 @@
 class Address < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフンが必要です。' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city
-    validates :lot_number
-    validates :phone, format: { with: /\A[0-9]{,11}+\z/, message: 'は11桁以下の半角数字で入力して下さい。' }
-  end
-
   belongs_to :order
 end
 
