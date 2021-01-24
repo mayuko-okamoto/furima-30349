fb6453eca356eab9484dfb1bfc49495d2d8bc32e
mayuko-okamoto <astrology0928@icloud.com>
2021-01-24 17:04:51 +0900

    orderコントローラー、ルーティング設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,12 @@
 class OrdersController < ApplicationController
+
+  def index
+    @user_order = UserOrder.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    binding.pry
+  end
+
 end
