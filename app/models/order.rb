0505685dda5a2a83cdf55130b54ba0b5371c7ad4
mayuko-okamoto <astrology0928@icloud.com>
2021-01-24 12:26:58 +0900

    addressモデル、orderモデル作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,28 +1,5 @@
-class Order
-  include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :lot_number, :building_name, :phone
-  
-  with_options presence: true do
-    validates :postal_code,
-              format: { with: /\A\d{3}[-]\d{4}\z/, message: 'はハイフンが必要です。' }
-    validates :prefecture_id
-    validates :city
-    validates :lot_number
-    validates :phone, 
-              format: { with: /\A[0-9]{,8}+\z/, message: 'は11桁以下の半角数字で入力して下さい。' }
-  end
+class Order < ApplicationRecord
 
-  def save
-    # 各テーブルにデータを保存する処理を書く
-  end
+
+
 end
-
-
-| Column          | Type       | Options                                    |
-|-----------------|------------|--------------------------------------------|
-| postal_code     | string     | null: false,format: { with: /\A[0-9]+\z/ } |
-| prefecture_id   | integer    | null: false                                |
-| city            | string     | null: false                                |
-| lot_number      | string     | null: false                                |
-| building_name   | string     |                                            |
-| phone           | string     | null: false,format: { with: /\A[0-9]+\z/ } |
