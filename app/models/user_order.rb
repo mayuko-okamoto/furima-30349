3936da5d99a38c2dfa09883b2cbc5c13ee37aa39
mayuko-okamoto <astrology0928@icloud.com>
2021-01-31 13:32:56 +0900

    エラーメッセージ表示

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,6 +1,6 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :lot_number, :building_name, :phone, :orders_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :lot_number, :building_name, :phone, :order_id, :user_id, :item_id
   
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフンが必要です。' }
